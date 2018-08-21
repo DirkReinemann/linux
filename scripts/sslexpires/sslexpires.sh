e826acd4480a4bd55ad09d6d8a395389cd67e8b3
Dirk Reinemann <dirk.reinemann@gmx.de>
2018-08-21 23:09:28 +0200

    Updated

--- scripts/sslexpires/sslexpires.sh
+++ scripts/sslexpires/sslexpires.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-DOMAINS=(www.dirkreinemann.com owncloud.dirkreinemann.com blog.dirkreinemann.com wiki.dirkreinemann.com zuendstoff.dirkreinemann.com etherpad.dirkreinemann.com)
+DOMAIN="antinet.ddnss.de"
 
-for DOMAIN in "${DOMAINS[@]}"; do
-    EXPIRES=$(curl --insecure --head -v https://$DOMAIN 2>&1 | grep expire | grep -o 'date: .*')
-    echo "$DOMAIN: $EXPIRES"
+SUBDOMAINS=(
+"zuendstoff"
+"dav"
+)
+
+for SUBDOMAIN in "${SUBDOMAINS[@]}"; do
+    FQDN="${SUBDOMAIN}.${DOMAIN}"
+    EXPDATE="$( \
+        curl -k -I -v "https://$FQDN" 2>&1 | \
+            grep 'expire' | \
+            awk -F': ' '{ print $2 }'
+    )"
+    EXPDATEFORMAT="$(date --date="$EXPDATE" "+%d.%m.%Y %H:%M:%S")"
+    printf "%-50s %s\\n" "$FQDN" "$EXPDATEFORMAT"
 done
 
 exit 0
